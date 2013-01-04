043052992ca260c35bdf48250fdc3105631c1449
Joachim Glauche <webmaster@joaz.de>
2013-01-04 22:16:53 +0100

    horizontal shelf using alu profile and a cheap belt strap conveyor. WIP

--- storage/test1/storage.py
+++ storage/test1/storage.py
@@ -13,33 +13,110 @@ sys.path.append( superdir)
 from solid import *
 
 use("../../libs/tslot.scad")
+use("../../libs/standard.scad")
+use("../../libs/publicDomainGearV1.1.scad")
+ 
+def container(length):
+    r=cube([32,length,40])
+    r+=translate([-9,0,20])(cube([50,length,2]))
+    r-=translate([1.5,2.5,3])(cube([29,length-5,41]))
+    return color("lime")(r)
+
+def tslot_spacer(spacing):
+    r = cube([spacing+20,20,5])
+    r+= cube([20,20,10])    
+    r+= translate([spacing,0,0])(cube([20,20,10]))
+    
+    bolt_hole = translate([0,0,5.3])(cylinder(r=2.7,h=5.1))
+    bolt_hole += cylinder(r=4.4,h=5.1);
+    
+    r-= translate([10,10,-0.1])(bolt_hole)
+    r-= translate([10+spacing,10,-0.1])(bolt_hole)
+        
+    return r
+
+#res=tslot_spacer(55)
+
+def drive_bearing_holder():
+    r = cube([20,30,28])
+    
+    # bearing and shaft clearance
+    r -= translate([-0.1,15,14])(rotate([0,90,0])(cylinder(r=7,h=100)))
+    r -= translate([14.9,15,14])(rotate([0,90,0])(cylinder(r=11.2,h=7.2)))
+    
+    # mounting hole 
+    r-= translate([10,15,-0.1])(rotate([0,0,0])(cylinder(r=2.7,h=100)))
+    r-= translate([10,15,-0.1])(rotate([0,0,0])(cylinder(r=4.4,h=25)))
+    
+    
+    return r
+#res = rotate([0,-90,0])(drive_bearing_holder())
+
+def storage_shelf(length,spacing):	
+    r = tslot20(length,0)
+    r += translate([spacing,0,0])(tslot20(length,0))    
+    r = color("blue")(r)
+    r = rotate([-90,0,0])(translate([0,0,0])(r))
+
+    r += translate([0,30,-30])(tslot_spacer(spacing))
+    r += translate([0,length-20,-30])(tslot_spacer(spacing))
+
+
+    g = gear(mm_per_tooth=1.0*3.14159,thickness=25,number_of_teeth=23,hole_diameter=8.4)
+    g = translate([38,0,-34])(rotate([0,90,0])(g))
+    r += translate([22,0,-20])(container(60))
+    r += g	
+    #r += translate([0,400,0])(g)	
+    
+    r += translate([0,-15,-48])(drive_bearing_holder())
+
+    r += translate([15+0.3,0,-34])(rotate([0,90,0])(bearing_608()))
+    r += translate([spacing-0.3,0,-34])(rotate([0,90,0])(bearing_608()))
+    
+    # conveyor dummy
+    r+= color("silver")(translate([25,0,-21])(cube([25,430,1])))
+    r+= color("silver")(translate([25,0,-21-27])(cube([25,430,1])))
+    
+    return r	
+
+res = storage_shelf(400,55)
+
+def belt_roller(height, inner_dia):
+    r = cylinder(r=12,h=height,segments=64);
+    r-= cylinder(r=inner_dia/2,h=height,segments=64);
+    r-= translate([2,-1.5,0])(cube([height,3,height]))
+    return r
+
+def conveyor_clamp(belt_width):
+    r = cube([belt_width+13, 20, 5])
+    # zip tie holes
+    r -= translate([5,5,-0.1])(cube([4,2,100]))
+    r -= translate([5+belt_width,5,-0.1])(cube([4,2,100]))
+    # clamp holes
+    r -= translate([1.5+2.5,15,-0.1])(cylinder(r=1.7,h=100,segments=64))
+    r -= translate([1.5+7.5+belt_width,15,-0.1])(cylinder(r=1.7,h=100,segments=64))
+    # nut traps
+    r -= translate([1.5+2.5,15,3])(cylinder(r=3.3,h=4,segments=6))
+    r -= translate([1.5+7.5+belt_width,15,3])(cylinder(r=3.3,h=4,segments=6))
+    
+    # second part
+    r += translate([0,22,0])(cube([belt_width+13, 10, 3 ]))
+    r -= translate([1.5+2.5,27,-0.1])(cylinder(r=1.7,h=100,segments=64))
+    r -= translate([1.5+7.5+belt_width,27,-0.1])(cylinder(r=1.7,h=100,segments=64))
+
+    
+    # belt
+    #r+= color("silver")(translate([1.5+5,0,0])(cube([belt_width,100,1])))
+    return r
+
+    
 
 
 
-def storage_subunit(length,spacing):	
-	r = tslot20(length,0)
-	r += translate([spacing,0,0])(tslot20(length,0)) 
-	
-	return color("blue")(translate([20,0,0])(r))	
+#res = belt_roller(30, 8.6) 
 
-
-def storage_unit(x,y,length,spacing, height):
-	r = cube([0,0,0])	
-	for f in range(y):	
-		for i in range(x):
-			r += translate([i*(spacing+40),0,0])(storage_subunit(length,spacing))
-			r += translate([spacing+40+i*(spacing+40),0,0])(rotate([90,0,0])(tslot20(400,0)))
-
-	
-	return r
-	
-#	r = rotate([90,0,0])(tslot20(400,0))
-#	return color("red")(r)
-	
-
-#res = storage_subunit(400,50)
-res = storage_unit(3,3,400,50,60)
-
-
+#res = conveyor_clamp(25) 
+ 
+ 
  
 print scad_render(res)
